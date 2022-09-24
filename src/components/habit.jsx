7e8fa4a569cf482a7340947a09fe02a1b3128f43
KyongRyul Na <skrudfbf0813@naver.com>
2022-09-24 19:00:43 +0900

    habit component

---
+++ src/components/habit.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트
+    this.setState({ count: this.state.count + 1 });
+    // this.state.count++;
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
+  render() {
+    return (
+      <li className="habit">
+        <span className="habit-name">Reading</span>
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fa-solid fa-square-plus"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fa-solid fa-square-minus"></i>
+        </button>
+        <button className="habit-button habit-delete">
+          <i className="fa-solid fa-trash"></i>{" "}
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
