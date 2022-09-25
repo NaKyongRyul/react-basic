5b5faf4e672aa500865351897b5b417abef54408
KyongRyul Na <skrudfbf0813@naver.com>
2022-09-25 09:45:15 +0900

    habit component

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
   handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   };
-
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
   };
-
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
   render() {
     const { name, count } = this.props.habit;
     return (
@@ -33,7 +28,10 @@ class Habit extends Component {
         >
           <i className="fa-solid fa-square-minus"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fa-solid fa-trash"></i>{" "}
         </button>
       </li>
