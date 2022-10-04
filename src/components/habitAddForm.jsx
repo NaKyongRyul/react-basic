cfb0194d77ba76b27b0cdc6b527ccffad62ae0e0
KyongRyul Na <skrudfbf0813@naver.com>
2022-10-04 16:29:02 +0900

    habitAddForm component

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,28 +1,24 @@
-import React, { Component } from "react";
+import React, { memo } from "react";
 
-class HabitAddForm extends Component {
-  inputRef = React.createRef();
-
-  handleSubmit = (event) => {
+const HabitAddForm = memo((props) => {
+  const inputRef = React.createRef();
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    // name이 빈칸이 아니라면 onAdd에 이름을 전달
-    name && this.props.onAdd(name);
-    this.inputRef.current.value = "";
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    inputRef.current.value = "";
   };
-  render() {
-    return (
-      <form className="add-form" onSubmit={this.handleSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="Habit"
-        ></input>
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      ></input>
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default HabitAddForm;
